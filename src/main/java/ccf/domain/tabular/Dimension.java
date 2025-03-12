6c9e261be01ec25c2061085f1a2e80d6fb992cfb
venkykrishnan <venky@stratifytech.com>
2025-03-12 16:14:05 -0700

    WOrking on dimension

--- src/main/java/ccf/domain/tabular/Dimension.java
+++ src/main/java/ccf/domain/tabular/Dimension.java
@@ -15,46 +15,16 @@ import java.util.stream.Stream;
 // HIA: 10 Mar 25 TODO work on standard dimensions & types
 public record Dimension(String name, String description) {
 }
-public record Bank(String bankId,
-                   ccf.domain.bank.Bank.BankMetadata metadata,
-                   BankInstanceType instanceType,
-                   List<String> users,
-                   BankStatus status,
-                   Instant creationTimestamp, Instant modificationTimestamp
-) {
-    private static final Logger logger = LoggerFactory.getLogger(ccf.domain.bank.Bank.class);
 
-    public record NewUser(String userId) {
-    }
-
-    // First time bank record. This is also used in views
-    public record BankCreateInfo(String bankId,
-                                 ccf.domain.bank.Bank.BankMetadata metadata,
-                                 BankInstanceType instanceType,
-                                 List<String> users,
-                                 BankStatus status,
-                                 Instant creationTimestamp, Instant modificationTimestamp) {
-    }
-
-    public record BankMetadata(Integer naicsCode, URL url) {
-    }
-
-    public ccf.domain.bank.Bank onBankCreated(BankEvent.BankCreated bankCreated) {
-        ccf.domain.bank.Bank.BankCreateInfo createInfo = bankCreated.createInfo();
-        return new ccf.domain.bank.Bank(createInfo.bankId, createInfo.metadata(),
-                createInfo.instanceType, createInfo.users,
-                createInfo.status,
-                createInfo.creationTimestamp, createInfo.modificationTimestamp);
-    }
-
-    public ccf.domain.bank.Bank onBankUserAdded(BankEvent.BankUserAdded bankUserAdded) {
-        logger.info("Adding user to bank id={} metadata= {}, userId={}", bankId, metadata,bankUserAdded.userId());
-        List<String> updatedUsers = Stream.concat(
-                Optional.ofNullable(users).stream().flatMap(List::stream),
-                Stream.of(bankUserAdded.userId())
-        ).toList();
-
-        return new ccf.domain.bank.Bank(bankId, metadata, instanceType, updatedUsers,
-                BankStatus.BANK_INITIALIZED, creationTimestamp, Instant.now());
-    }
+public record DimensionValue(String name, String description, String type, String parent) {
 }
+
+public record DimensionValues(List<DimensionValue> values) {
+}
+public record DimensionWithValues(Dimension dimension, DimensionValues values) {
+}
+
+public record DimensionType(String name, String description, String parent) {
+}
+
+
